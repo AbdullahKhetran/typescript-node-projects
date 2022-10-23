b291eb23ee49873812c5e370bd9e2d79e4489ddf
Abdullah Khetran <khetranabdullah@gmail.com>
2022-10-23 07:01:32 +0500

    added code for 01_number_guessing_game

---
+++ 01_number_guessing_game/index.mjs
@@ -0,0 +1,31 @@
+import inquirer from 'inquirer';
+let num = Math.random();
+num = num * 10;
+num += 1;
+num = Math.floor(num);
+console.log(num);
+inquirer
+    .prompt([
+    {
+        type: "input",
+        name: "choice",
+        message: "Guess the number computer choose from 1 to 10",
+        validate: (answer) => {
+            if (isNaN(answer)) {
+                return "Please enter a valid number";
+            }
+            else if (answer > 10 || answer < 1) {
+                return "Hint: Number is between 1 (inclusive) and 10";
+            }
+            return true;
+        }
+    }
+])
+    .then((answers) => {
+    if (answers.choice == num) {
+        console.log("Bravo! You guessed the number correctly");
+    }
+    else {
+        console.log("Wrong answer. Better luck next time");
+    }
+});
