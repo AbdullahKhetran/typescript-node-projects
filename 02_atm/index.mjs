f4a904a05dd4069d1bcc1abc7ef3ca6b419e2a71
Abdullah Khetran <khetranabdullah@gmail.com>
2022-10-23 07:46:58 +0500

    added code for 02_atm

---
+++ 02_atm/index.mjs
@@ -0,0 +1,41 @@
+import inquirer from 'inquirer';
+let userID = Math.floor((Math.random() * 100) + 1);
+console.log(userID);
+let userPin = Math.floor((Math.random() * 1000) + 1);
+console.log(userPin);
+inquirer
+    .prompt([
+    {
+        type: "input",
+        name: "id",
+        message: "Please enter your ID",
+        validate: (answer) => {
+            if (isNaN(answer)) {
+                return "Please enter a valid number";
+            }
+            else if (answer != userID) {
+                return "Invalid User ID";
+            }
+            return true;
+        }
+    },
+    {
+        type: "input",
+        name: "pin",
+        message: "Please enter your PIN",
+        validate: (answer) => {
+            if (isNaN(answer)) {
+                return "Please enter a valid number";
+            }
+            else if (answer != userPin) {
+                return "Invalid PIN";
+            }
+            return true;
+        }
+    }
+])
+    .then((answers) => {
+    if (answers.id == userID && answers.pin == userPin) {
+        console.log("Welcome back!");
+    }
+});
